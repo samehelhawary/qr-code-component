41724529991109d4b1bcc85cd042456b532e0b07
samehelhawary <basio20044@gmail.com>
2024-10-22 10:37:27 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Outfit', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
